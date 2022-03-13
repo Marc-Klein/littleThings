0f8e464ac234beb9724845424e4720600187b4ec
Marc Klein <marc.klein1989@gmail.com>
2022-03-13 17:58:09 +0100

    chore: centered basic navigation

--- src/organisms/layout/index.jsx
+++ src/organisms/layout/index.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import Footer from "../footer";
 import Header from "../header";
+import styled from "@emotion/styled";
+
+const CenteredMain = styled.main`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100vw;
+	height: 100vh;
+`;
 
 const Layout = ({ children }) => {
 	return (
 		<>
 			<Header />
-			<main>{children}</main>
+			<CenteredMain>{children}</CenteredMain>
 			<Footer />
 		</>
 	);
