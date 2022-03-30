14061704cef9ca57897a50fe369bcac65773a99b
Marc Klein <marc.klein1989@gmail.com>
2022-03-30 13:05:59 +0200

    chore: added new appbar

--- src/organisms/header/index.jsx
+++ src/organisms/header/index.jsx
@@ -1,32 +1,43 @@
-import Link from "next/link";
 import React from "react";
-import styled from "@emotion/styled";
-
-const StyledLink = styled(Link)`
-	//test
-`;
-
-const StyledHeader = styled.header`
-	display: flex;
-	height: 40px;
-`;
-
-const StyledNav = styled.nav`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	width: 100%;
-	background: black;
-	color: aliceblue;
-`;
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useState } from "react";
 
 const Header = () => {
+	const [isOpen, setOpen] = useState(0);
+	const [isClosed, setClosed] = useState(0);
+
+	const handleOpen = event => {
+		setOpen(event.currentTarget);
+	};
+
+	const handleClose = event => {
+		setClosed(event.currentTarget);
+	};
+
 	return (
-		<StyledHeader>
-			<StyledNav data-test-id="navigation">
-				<Link href="/">Home</Link>
-			</StyledNav>
-		</StyledHeader>
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar position="static">
+				<Toolbar variant="dense">
+					<IconButton
+						edge="start"
+						color="inherit"
+						aria-label="menu"
+						sx={{ mr: 2 }}
+						onClick={handleOpen}
+					>
+						<MenuIcon />
+					</IconButton>
+					<Typography variant="h6" color="inherit" component="div">
+						Little Projects
+					</Typography>
+				</Toolbar>
+			</AppBar>
+		</Box>
 	);
 };
 
